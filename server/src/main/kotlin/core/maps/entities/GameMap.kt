47ff2089d57d060fea1f48fa9f8dd9c1ec8b531e
Humberd <humberd2@gmail.com>
2021-04-05 12:12:46 +0200

    Server: Move GameMapCreaturesContainer to a separate file

--- server/src/main/kotlin/core/maps/entities/GameMap.kt
+++ server/src/main/kotlin/core/maps/entities/GameMap.kt
@@ -4,7 +4,6 @@ import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
 import core.maps.entities.creatures.Creature
-import core.maps.entities.creatures.player.Player
 import core.maps.shapes.Wall
 import core.types.*
 import mu.KotlinLogging
@@ -18,75 +17,7 @@ class GameMap(
     val gridHeight: Int,
     private val grid: Array<Array<Tile>>,
 ) {
-    //region Creatures Store
-    val creatures = CreaturesContainer()
-
-    inner class CreaturesContainer {
-        private val players = HashMap<PID, Player>()
-        private val creatures = HashMap<CID, Creature>()
-
-        fun add(creature: Creature) {
-            if (creatures.containsKey(creature.cid)) {
-                throw Error("Creature already exists")
-            }
-
-            if (creature is Player) {
-                if (players.containsKey(creature.pid)) {
-                    throw Error("Player already exists")
-                }
-                players[creature.pid] = creature
-            }
-
-            creatures[creature.cid] = creature
-
-            creature.onInit()
-            creature.hooks.onAddedToMap(this@GameMap)
-        }
-
-        fun remove(pid: PID) {
-            if (!players.containsKey(pid)) {
-                throw Error("Player doesn't exist")
-            }
-
-            remove(players[pid]!!.cid)
-            val player = players.remove(pid)
-            getTileAt(player!!.lastUpdate.gridPosition).creatures.remove(player!!.cid)
-        }
-
-        fun remove(cid: CID) {
-            if (!creatures.containsKey(cid)) {
-                throw Error("Creature doesn't exist")
-            }
-            val removedCreature = creatures.remove(cid)
-            removedCreature!!.hooks.onRemovedFromMap(this@GameMap)
-        }
-
-        fun get(pid: PID): Player {
-            return players[pid] ?: throw Error("Player not found")
-        }
-
-        fun get(cid: CID): Creature {
-            return creatures[cid] ?: throw Error("Creature not found")
-        }
-
-        fun getAllPlayers(): Collection<Player> {
-            return players.values
-        }
-
-        fun getAllCreatures(): Collection<Creature> {
-            return creatures.values
-        }
-
-        fun moveTo(pid: PID, targetPosition: WorldPosition) {
-            moveTo(get(pid).cid, targetPosition)
-        }
-
-        fun moveTo(cid: CID, targetPosition: WorldPosition) {
-            val creature = get(cid)
-            creature.movement.startMovingTo(targetPosition)
-        }
-    }
-    //endregion
+    val creatures = GameMapCreaturesContainer(this)
 
     //region Physics Initialization
     val physics: World
