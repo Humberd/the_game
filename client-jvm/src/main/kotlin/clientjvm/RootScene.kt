bd566a18732bcc31f4ec76529365e8e233370de9
humberd <humberd2@gmail.com>
2021-04-24 12:13:16 +0200

    Save commit

--- client-jvm/src/main/kotlin/clientjvm/RootScene.kt
+++ client-jvm/src/main/kotlin/clientjvm/RootScene.kt
@@ -7,19 +7,19 @@ import godot.annotation.RegisterFunction
 @RegisterClass
 class RootScene : Spatial() {
 
-	// Declare member variables here. Examples:
-	// val a = 2;
-	// val b = "text";
+    // Declare member variables here. Examples:
+    // val a = 2;
+    // val b = "text";
 
-	// Called when the node enters the scene tree for the first time.
-	@RegisterFunction
-	override fun _ready() {
-		println("heeheh")
-	}
+    // Called when the node enters the scene tree for the first time.
+    @RegisterFunction
+    override fun _ready() {
+        println("heeheh")
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	@RegisterFunction
-	override fun _process(delta: Double) {
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    @RegisterFunction
+    override fun _process(delta: Double) {
 
-	}
+    }
 }
