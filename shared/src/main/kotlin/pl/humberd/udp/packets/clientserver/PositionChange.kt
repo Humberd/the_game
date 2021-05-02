bf9e5d29fda7d54fe8569529b5cccd9f4282a44a
humberd <humberd2@gmail.com>
2021-05-02 11:25:17 +0200

    Add hot packet

--- shared/src/main/kotlin/pl/humberd/udp/packets/clientserver/PositionChange.kt
+++ shared/src/main/kotlin/pl/humberd/udp/packets/clientserver/PositionChange.kt
@@ -1,10 +1,12 @@
 package pl.humberd.udp.packets.clientserver
 
+import pl.humberd.misc.HotPacket
 import pl.humberd.models.ApiVector2
 import pl.humberd.udp.packets.ReadBuffer
 import pl.humberd.udp.packets.WriteBuffer
 import pl.humberd.udp.packets.clientserver.ClientServerUdpPacket.Type.POSITION_CHANGE
 
+@HotPacket
 data class PositionChange(
     val targetPosition: ApiVector2
 ) : ClientServerUdpPacket(POSITION_CHANGE) {
