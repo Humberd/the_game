42f4e56901034b4ee1e91ab21853568db6a48d62
humberd <humberd2@gmail.com>
2021-05-02 13:47:14 +0200

    Now also drawing debug stuff

--- client-jvm/src/main/kotlin/clientjvm/scenes/game/scenes/gameviewport/scenes/terrain/TerrainScene.kt
+++ client-jvm/src/main/kotlin/clientjvm/scenes/game/scenes/gameviewport/scenes/terrain/TerrainScene.kt
@@ -3,10 +3,7 @@ package clientjvm.scenes.game.scenes.gameviewport.scenes.terrain
 import clientjvm.exts.*
 import clientjvm.global.ClientDataReceiver
 import clientjvm.scenes.game.scenes.gameviewport.scenes.terrain.scenes.ground_tile.GroundTileScene
-import godot.Area
-import godot.ArrayMesh
-import godot.Mesh
-import godot.MeshInstance
+import godot.*
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
 import godot.core.*
@@ -60,13 +57,14 @@ class TerrainScene : Area() {
                 val offsetY = y - startY
                 val index = offsetX * packet.windowHeight.toShort() + offsetY
                 val spriteId = packet.spriteIds.get(index)
-//                tiles[x][y].setTile(spriteId)
+                tiles[x][y].setTile(spriteId)
             }
         }
     }
 
     fun drawWalls(packet: TerrainWallsUpdate) {
         logger.info { packet }
+
         for (chain in listOf(packet.chains.last())) {
             logger.info { chain.print() }
             val baseChain = variantArrayOf(*Array(chain.size) { chain[it].convert().to3D() })
@@ -78,34 +76,38 @@ class TerrainScene : Area() {
                 }
             }
 
-            val colors = variantArrayOf(*Array(chain.size) { Color.blue })
-            val poolColors = PoolColorArray().also {
-                for (i in 0 until baseChain.size) {
-                    it.append(Color.blue)
-                }
-            }
-            logger.info { topChain.print() }
-            logger.info { colors.print() }
 
             val arrayMesh = ArrayMesh().also {
                 it.addSurfaceFromArrays(
                     primitive = Mesh.PrimitiveType.PRIMITIVE_TRIANGLE_FAN.id,
-                    arrays = surfaceArray(
-                        ARRAY_VERTEX = topChain,
-                    )
+                    arrays = surfaceArray(ARRAY_VERTEX = topChain)
+                )
+                it.addSurfaceFromArrays(
+                    primitive = Mesh.PrimitiveType.PRIMITIVE_LINE_STRIP.id,
+                    arrays = surfaceArray(ARRAY_VERTEX = topChain)
                 )
                 it.addSurfaceFromArrays(
                     primitive = Mesh.PrimitiveType.PRIMITIVE_TRIANGLE_STRIP.id,
                     arrays = surfaceArray(ARRAY_VERTEX = sidesChain)
                 )
-                println(it.getSurfaceCount())
+                it.addSurfaceFromArrays(
+                    primitive = Mesh.PrimitiveType.PRIMITIVE_LINE_STRIP.id,
+                    arrays = surfaceArray(ARRAY_VERTEX = sidesChain)
+                )
             }
 
             val meshInstance = MeshInstance().also {
                 it.mesh = arrayMesh
                 it.name = "__wall"
+                it.setSurfaceMaterial(1, SpatialMaterial().also {
+                    it.albedoColor = Color.black
+                })
+                it.setSurfaceMaterial(3, SpatialMaterial().also {
+                    it.albedoColor = Color.black
+                })
             }
             addChild(meshInstance)
+            meshInstance.createDebugTangents()
         }
     }
 
