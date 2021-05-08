8fbefab31e8edaaec1cae27cebefd8969bad4133
humberd <humberd2@gmail.com>
2021-05-08 12:10:12 +0200

    Cleanup

--- client-jvm/src/main/kotlin/clientjvm/scenes/game/scenes/gameviewport/scenes/creatures/scenes/creature/PlayerController.kt
+++ client-jvm/src/main/kotlin/clientjvm/scenes/game/scenes/gameviewport/scenes/creatures/scenes/creature/PlayerController.kt
@@ -1,16 +1,16 @@
 package clientjvm.scenes.game.scenes.gameviewport.scenes.creatures.scenes.creature
 
-import clientjvm.exts.*
+import clientjvm.exts.castCameraRays
+import clientjvm.exts.convert
+import clientjvm.exts.emitter
+import clientjvm.exts.to2D
 import clientjvm.global.ClientDataSender
 import clientjvm.global.CollisionLayer
-import clientjvm.global.CrossScenesManager
 import clientjvm.global.GodotWorker
 import godot.InputEvent
 import godot.Spatial
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
-import godot.core.Vector3
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.subjects.PublishSubject
 import mu.KLogging
 import pl.humberd.udp.packets.clientserver.PositionChange
@@ -30,10 +30,9 @@ class PlayerController : Spatial() {
         positionChangeStream
             .throttleLast(200, TimeUnit.MILLISECONDS)
             .observeOn(GodotWorker)
-            .switchMap { sendPositionChange() }
             .takeUntil(unsub)
             .subscribe {
-                ClientDataSender.send(PositionChange(it[0].to2D().convert()))
+                sendPositionChange()
             }
     }
 
@@ -59,7 +58,7 @@ class PlayerController : Spatial() {
         unsub.onNext(true)
     }
 
-    private fun sendPositionChange(): Observable<Array<Vector3>> {
+    private fun sendPositionChange() {
         val result = castCameraRays(
             collideWithAreas = false,
             collideWithBodies = true,
@@ -67,15 +66,10 @@ class PlayerController : Spatial() {
         )
 
         if (result == null) {
-            return Observable.empty()
+            return
         }
 
-        val promise = Promise<Pair<Vector3, Vector3>, Array<Vector3>>(
-            translation to result.position
-        )
-        CrossScenesManager.positionChangeRequest.onNext(promise)
-
-        return promise.emitter
+        ClientDataSender.send(PositionChange(result.position.to2D().convert()))
     }
 }
 
--- client-jvm/src/main/kotlin/clientjvm/scenes/game/scenes/gameviewport/scenes/terrain/TerrainScene.kt
+++ client-jvm/src/main/kotlin/clientjvm/scenes/game/scenes/gameviewport/scenes/terrain/TerrainScene.kt
@@ -1,19 +1,14 @@
 package clientjvm.scenes.game.scenes.gameviewport.scenes.terrain
 
-import clientjvm.addons.godotnavigationlite.DetourNavigationMesh
-import clientjvm.exts.Promise
 import clientjvm.exts.emitter
 import clientjvm.global.ClientDataReceiver
-import clientjvm.global.CrossScenesManager
+import clientjvm.global.CollisionLayer
 import clientjvm.scenes.game.scenes.gameviewport.scenes.terrain.scenes.ground_tile.GroundTileScene
-import godot.BoxShape
-import godot.CollisionShape
-import godot.Spatial
+import godot.*
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
 import godot.core.Vector2
 import godot.core.Vector3
-import godot.getNode
 import mu.KLogging
 import pl.humberd.udp.packets.serverclient.TerrainUpdate
 
@@ -23,7 +18,6 @@ class TerrainScene : Spatial() {
         private const val GRID_SIZE = 20
     }
 
-    private lateinit var detourNavigationMesh: DetourNavigationMesh
     private lateinit var content: Spatial
     private lateinit var tiles: Array<Array<GroundTileScene>>
 
@@ -31,15 +25,16 @@ class TerrainScene : Spatial() {
 
     @RegisterFunction
     override fun _ready() {
-        detourNavigationMesh = DetourNavigationMesh(getNode("Navigation/DetourNavigationMesh"))
-        content = getNode("Navigation/Content")
+        content = getNode("Content")
 
-        getNode<CollisionShape>("Navigation/Content/Platform/Collider").also {
+        getNode<CollisionShape>("Content/Platform/Collider").also {
             val boxShape = it.shape as BoxShape
             val offset = GRID_SIZE / 2
             boxShape.extents = Vector3(offset, 0.001, offset)
             it.translation = Vector3(offset, 0, offset)
-            detourNavigationMesh.bakeNavmesh()
+            val platform = it.getParent()
+            require(platform is StaticBody)
+            platform.collisionLayer = CollisionLayer.TERRAIN_PLATFORM.value.toLong()
         }
 
         tiles = Array(GRID_SIZE) { x ->
@@ -54,14 +49,6 @@ class TerrainScene : Spatial() {
         ClientDataReceiver.watchFor<TerrainUpdate>()
             .takeUntil(unsub)
             .subscribe { drawTerrain(it) }
-
-//        ClientDataReceiver.watchFor<TerrainWallsUpdate>()
-//            .takeUntil(unsub)
-//            .subscribe { drawWalls(it) }
-
-        CrossScenesManager.positionChangeRequest
-            .takeUntil(unsub)
-            .subscribe { findPaths(it) }
     }
 
     fun drawTerrain(packet: TerrainUpdate) {
@@ -94,12 +81,5 @@ class TerrainScene : Spatial() {
 
     override fun _onDestroy() {
         unsub.onNext(true)
-    }
-
-    private fun findPaths(promise: Promise<Pair<Vector3, Vector3>, Array<Vector3>>) {
-        val (start, end) = promise.data
-        promise.resolve(arrayOf(end))
-        // fixme: https://github.com/utopia-rise/godot-kotlin-jvm/pull/219
-//        val response = detourNavigationMesh.findPath(start, end)
     }
 }
